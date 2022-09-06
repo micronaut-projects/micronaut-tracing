c8f8738ad3883f43b38d626a591091dcc2d8283f
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-06 12:23:19 +0200

    Avoid bean pollution
    - Don't use ReactorHttpClient
    - Use @Requires spec.name to avoid bean pollution
    - Annotate controller with @produces(MediaType.TEXT_PLAIN) and accept.

--- tracing-opentelemetry/src/test/groovy/io/micronaut/tracing/instrument/util/HttpClientResetForTestSpec.groovy
+++ tracing-opentelemetry/src/test/groovy/io/micronaut/tracing/instrument/util/HttpClientResetForTestSpec.groovy
@@ -2,10 +2,14 @@ package io.micronaut.tracing.instrument.util
 
 import groovy.util.logging.Slf4j
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Requires
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Header
+import io.micronaut.http.annotation.Produces
+import io.micronaut.http.client.HttpClient
 import io.micronaut.reactor.http.client.ReactorHttpClient
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.scheduling.annotation.ExecuteOn
@@ -26,30 +30,36 @@ class HttpClientResetForTestSpec extends Specification {
     @Shared
     @AutoCleanup
     EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, [
-            'micronaut.application.name': 'test-app'
+            'micronaut.application.name': 'test-app',
+            'spec.name': 'HttpClientResetForTestSpec'
     ])
 
     @Shared
     @AutoCleanup
-    ReactorHttpClient reactorHttpClient = ReactorHttpClient.create(embeddedServer.URL)
-
+    HttpClient httpClient = HttpClient.create(embeddedServer.URL)
 
     void 'test pre destroy resetForTest'() {
+        given:
         String tracingId = UUID.randomUUID()
 
-        expect:
+        when:
         HttpRequest<Object> request = HttpRequest
                 .GET("/test/test")
+                .accept(MediaType.TEXT_PLAIN)
                 .header("X-TrackingId", tracingId)
-        def resp = reactorHttpClient.toBlocking().retrieve(request)
+        String resp = httpClient.toBlocking().retrieve(request)
+
+        then:
+        noExceptionThrown()
         resp == tracingId
     }
 
+    @Requires(property = "spec.name", value = "HttpClientResetForTestSpec")
     @Controller("/test")
     static class TestController {
 
-
         @ExecuteOn(IO)
+        @Produces(MediaType.TEXT_PLAIN)
         @Get("/test")
         @ContinueSpan
         Mono<String> test(@SpanAttribute("tracing-annotation-span-attribute")

d7495082a5a42fcdecb0ee4b6a74c1c74e90e25a
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-12 16:05:50 +0200

    continue Nested HTTP Tracing Reactive Tests fail

--- tracing-jaeger/src/test/groovy/io/micronaut/tracing/jaeger/HttpTracingSpec.groovy
+++ tracing-jaeger/src/test/groovy/io/micronaut/tracing/jaeger/HttpTracingSpec.groovy
@@ -18,7 +18,6 @@ import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.scheduling.annotation.ExecuteOn
 import io.micronaut.tracing.annotation.ContinueSpan
-import io.micronaut.tracing.annotation.NewSpan
 import io.opentracing.Tracer
 import jakarta.inject.Inject
 import org.reactivestreams.Publisher
@@ -26,7 +25,6 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 import spock.lang.AutoCleanup
-import spock.lang.PendingFeature
 import spock.lang.Specification
 import spock.util.concurrent.PollingConditions
 
@@ -627,7 +625,6 @@ class HttpTracingSpec extends Specification {
         }
     }
 
-    @PendingFeature
     void 'test continue nested HTTP tracing - reactive'() {
         when:
         HttpResponse<String> response = client.toBlocking().exchange('/traced/nestedReactive/John', String)

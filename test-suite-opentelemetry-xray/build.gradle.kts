2259b7599bb0924e1c852cf5e7dfc422ce8d124d
Sergio del Amo <sergio.delamo@softamo.com>
2024-04-15 12:22:19 +0200

    test: adds a test-suite-xray module
    This adds a test-suite-xray module to verify the following dependencies are resolvables without specifying the version:
    
    io.opentelemetry:opentelemetry-exporter-otlp
    io.opentelemetry:opentelemetry-extension-aws
    io.opentelemetry.instrumentation:opentelemetry-aws-sdk-2.2

---
+++ test-suite-opentelemetry-xray/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java-library")
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testRuntimeOnly(mnLogging.logback.classic)
+
+    testImplementation(projects.micronautTracingOpentelemetryHttp)
+    testImplementation(libs.opentelemetry.exporter.otlp)
+    testImplementation(libs.opentelemetry.aws.sdk)
+    testImplementation(libs.opentelemetry.extension.aws)
+
+    testRuntimeOnly(libs.junit.engine)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testImplementation(mnTest.micronaut.test.junit5)
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+    targetCompatibility = JavaVersion.toVersion("17")
+}

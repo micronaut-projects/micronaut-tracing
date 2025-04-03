e924c9fd7de60d58420cc9e61d23c4fb247cdd5b
lgathy <gathy.lajos@gmail.com>
2025-04-04 00:50:16 +0100

    Make OTel Instrumenter beans fully customizable via their factory methods

--- tracing-opentelemetry-http/src/main/java/io/micronaut/tracing/opentelemetry/instrument/http/server/MicronautHttpServerTelemetryFactory.java
+++ tracing-opentelemetry-http/src/main/java/io/micronaut/tracing/opentelemetry/instrument/http/server/MicronautHttpServerTelemetryFactory.java
@@ -19,20 +19,31 @@ import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Prototype;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Nullable;
+import io.micronaut.core.annotation.Order;
+import io.micronaut.core.order.Ordered;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.ContextCustomizer;
+import io.opentelemetry.instrumentation.api.instrumenter.ErrorCauseExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
+import io.opentelemetry.instrumentation.api.instrumenter.OperationListener;
+import io.opentelemetry.instrumentation.api.instrumenter.OperationMetrics;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanLinksExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanNameExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanStatusExtractor;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpServerAttributesExtractor;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpServerAttributesExtractorBuilder;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpServerMetrics;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpServerRoute;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpSpanNameExtractor;
 import io.opentelemetry.instrumentation.api.semconv.http.HttpSpanStatusExtractor;
+import io.opentelemetry.sdk.common.internal.OtelVersion;
 import jakarta.inject.Named;
 import jakarta.inject.Qualifier;
+import jakarta.inject.Singleton;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
@@ -52,7 +63,7 @@ public final class MicronautHttpServerTelemetryFactory {
     private static final String INSTRUMENTATION_NAME = "io.micronaut.http.server";
 
     /**
-     * Server Qualifier represents AttributesExtractor that should be used in http srver instrumenter.
+     * Server Qualifier represents AttributesExtractor that should be used in http server instrumenter.
      */
     @Qualifier
     @Documented
@@ -62,26 +73,68 @@ public final class MicronautHttpServerTelemetryFactory {
     /**
      * Builds the http server Open Telemetry instrumenter.
      * @param openTelemetry the {@link OpenTelemetry}
-     * @param extractors the list of {@link AttributesExtractor}
+     * @param spanNameExtractor the {@link SpanNameExtractor}
+     * @param spanStatusExtractor the {@link SpanStatusExtractor}, can be {@code null}
+     * @param errorCauseExtractor the {@link ErrorCauseExtractor}, can be {@code null}
+     * @param attributesExtractors the list of {@link AttributesExtractor}
+     * @param contextCustomizers the list of {@link ContextCustomizer}
+     * @param operationMetrics the list of {@link OperationMetrics}
+     * @param operationListeners the list of {@link OperationListener}
+     * @param spanLinksExtractors the list of {@link SpanLinksExtractor}
      * @return the http server Open Telemetry instrumenter
      */
     @Prototype
     @Requires(beans = OpenTelemetry.class)
+    @SuppressWarnings("DuplicatedCode")
     @Named("micronautHttpServerTelemetryInstrumenter")
-    public Instrumenter<HttpRequest<Object>, HttpResponse<Object>> instrumenter(OpenTelemetry openTelemetry, @Server List<AttributesExtractor<HttpRequest<?>, HttpResponse<?>>> extractors) {
-        MicronautHttpServerAttributesGetter httpAttributesGetter = MicronautHttpServerAttributesGetter.INSTANCE;
+    public Instrumenter<HttpRequest<Object>, HttpResponse<Object>> instrumenter(
+        OpenTelemetry openTelemetry,
+        @Server SpanNameExtractor<HttpRequest<?>> spanNameExtractor,
+        @Server @Nullable SpanStatusExtractor<HttpRequest<?>, HttpResponse<?>> spanStatusExtractor,
+        @Server @Nullable ErrorCauseExtractor errorCauseExtractor,
+        @Server List<AttributesExtractor<HttpRequest<?>, HttpResponse<?>>> attributesExtractors,
+        @Server List<ContextCustomizer<HttpRequest<?>>> contextCustomizers,
+        @Server List<OperationMetrics> operationMetrics,
+        @Server List<OperationListener> operationListeners,
+        @Server List<SpanLinksExtractor<HttpRequest<Object>>> spanLinksExtractors) {
 
         InstrumenterBuilder<HttpRequest<Object>, HttpResponse<Object>> builder =
-            Instrumenter.builder(openTelemetry, INSTRUMENTATION_NAME,
-                HttpSpanNameExtractor.create(httpAttributesGetter));
+            Instrumenter.builder(openTelemetry, INSTRUMENTATION_NAME, spanNameExtractor);
 
-        builder.addAttributesExtractors(extractors);
+        builder.setInstrumentationVersion(OtelVersion.VERSION);
+        if (spanStatusExtractor != null) {
+            builder.setSpanStatusExtractor(spanStatusExtractor);
+        }
+        if (errorCauseExtractor != null) {
+            builder.setErrorCauseExtractor(errorCauseExtractor);
+        }
+        builder.addAttributesExtractors(attributesExtractors);
+        contextCustomizers.forEach(builder::addContextCustomizer);
+        operationListeners.forEach(builder::addOperationListener);
+        operationMetrics.forEach(builder::addOperationMetrics);
+        spanLinksExtractors.forEach(builder::addSpanLinksExtractor);
 
-        return builder
-            .setSpanStatusExtractor(HttpSpanStatusExtractor.create(httpAttributesGetter))
-            .addOperationMetrics(HttpServerMetrics.get())
-            .addContextCustomizer(HttpServerRoute.create(MicronautHttpServerAttributesGetter.INSTANCE))
-            .buildServerInstrumenter(HttpRequestGetter.INSTANCE);
+        return builder.buildServerInstrumenter(HttpRequestGetter.INSTANCE);
+    }
+
+    /**
+     * Builds the default {@link SpanNameExtractor}.
+     * @return the {@link HttpSpanNameExtractor}
+     */
+    @Server
+    @Singleton
+    SpanNameExtractor<HttpRequest<Object>> defaultSpanNameExtractor() {
+        return HttpSpanNameExtractor.create(MicronautHttpServerAttributesGetter.INSTANCE);
+    }
+
+    /**
+     * Builds the default {@link SpanStatusExtractor}.
+     * @return the {@link HttpSpanStatusExtractor}
+     */
+    @Server
+    @Singleton
+    SpanStatusExtractor<HttpRequest<Object>, HttpResponse<Object>> defaultSpanStatusExtractor() {
+        return HttpSpanStatusExtractor.create(MicronautHttpServerAttributesGetter.INSTANCE);
     }
 
     /**
@@ -100,5 +153,27 @@ public final class MicronautHttpServerTelemetryFactory {
             httpAttributesExtractorBuilder.setCapturedResponseHeaders(openTelemetryHttpServerConfig.getResponseHeaders());
         }
         return httpAttributesExtractorBuilder.build();
+    }
+
+    /**
+     * Returns an {@link OperationMetrics} instance which can be used to enable recording of {@link
+     * HttpServerMetrics}.
+     * @return the {@link OperationMetrics} instance
+     */
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Server
+    @Singleton
+    OperationMetrics httpServerMetrics() {
+        return HttpServerMetrics.get();
+    }
+
+    /**
+     * Builds a {@link ContextCustomizer} that initializes the {@code http.route} attribute value.
+     * @return the {@link ContextCustomizer}
+     */
+    @Server
+    @Singleton
+    ContextCustomizer<HttpRequest<Object>> httpServerRouteCustomizer() {
+        return HttpServerRoute.create(MicronautHttpServerAttributesGetter.INSTANCE);
     }
 }
--- tracing-opentelemetry/src/main/java/io/micronaut/tracing/opentelemetry/instrument/util/MicronautCodeTelemetryFactory.java
+++ tracing-opentelemetry/src/main/java/io/micronaut/tracing/opentelemetry/instrument/util/MicronautCodeTelemetryFactory.java
@@ -18,14 +18,30 @@ package io.micronaut.tracing.opentelemetry.instrument.util;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Prototype;
 import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.annotation.Nullable;
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.instrumentation.api.incubator.semconv.code.CodeAttributesGetter;
 import io.opentelemetry.instrumentation.api.incubator.semconv.code.CodeSpanNameExtractor;
 import io.opentelemetry.instrumentation.api.incubator.semconv.util.ClassAndMethod;
+import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.ContextCustomizer;
+import io.opentelemetry.instrumentation.api.instrumenter.ErrorCauseExtractor;
 import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
 import io.opentelemetry.instrumentation.api.instrumenter.InstrumenterBuilder;
-import io.opentelemetry.instrumentation.api.semconv.http.HttpClientMetrics;
+import io.opentelemetry.instrumentation.api.instrumenter.OperationListener;
+import io.opentelemetry.instrumentation.api.instrumenter.OperationMetrics;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanLinksExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanNameExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanStatusExtractor;
+import io.opentelemetry.sdk.common.internal.OtelVersion;
 import jakarta.inject.Named;
+import jakarta.inject.Qualifier;
+import jakarta.inject.Singleton;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.util.List;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * An HTTP client instrumentation factory for Open Telemetry.
@@ -39,19 +55,67 @@ public final class MicronautCodeTelemetryFactory {
     private static final String INSTRUMENTATION_NAME = "io.micronaut.code";
 
     /**
+     * Internal Qualifier represents AttributesExtractor that should be used in code instrumenter.
+     */
+    @Qualifier
+    @Documented
+    @Retention(RUNTIME)
+    public @interface Internal { }
+
+    /**
      * Builds the code Open Telemetry instrumenter.
      * @param openTelemetry the {@link OpenTelemetry}
+     * @param spanNameExtractor the {@link SpanNameExtractor}
+     * @param spanStatusExtractor the {@link SpanStatusExtractor}, can be {@code null}
+     * @param errorCauseExtractor the {@link ErrorCauseExtractor}, can be {@code null}
+     * @param attributesExtractors the list of {@link AttributesExtractor}
+     * @param contextCustomizers the list of {@link ContextCustomizer}
+     * @param operationMetrics the list of {@link OperationMetrics}
+     * @param operationListeners the list of {@link OperationListener}
+     * @param spanLinksExtractors the list of {@link SpanLinksExtractor}
      * @return the OpenTelemetry bean with default values
      */
     @Prototype
     @Requires(beans = OpenTelemetry.class)
+    @SuppressWarnings("DuplicatedCode")
     @Named("micronautCodeTelemetryInstrumenter")
-    public Instrumenter<ClassAndMethod, Object> instrumenter(OpenTelemetry openTelemetry) {
-        CodeAttributesGetter<ClassAndMethod> classAndMethodAttributesGetter = ClassAndMethod.codeAttributesGetter();
-        InstrumenterBuilder<ClassAndMethod, Object> builder = Instrumenter.builder(
-            openTelemetry, INSTRUMENTATION_NAME, CodeSpanNameExtractor.create(classAndMethodAttributesGetter));
+    public Instrumenter<ClassAndMethod, Object> instrumenter(
+        OpenTelemetry openTelemetry,
+        @Internal SpanNameExtractor<ClassAndMethod> spanNameExtractor,
+        @Internal @Nullable SpanStatusExtractor<ClassAndMethod, Object> spanStatusExtractor,
+        @Internal @Nullable ErrorCauseExtractor errorCauseExtractor,
+        @Internal List<AttributesExtractor<ClassAndMethod, Object>> attributesExtractors,
+        @Internal List<ContextCustomizer<ClassAndMethod>> contextCustomizers,
+        @Internal List<OperationMetrics> operationMetrics,
+        @Internal List<OperationListener> operationListeners,
+        @Internal List<SpanLinksExtractor<ClassAndMethod>> spanLinksExtractors) {
 
-        return builder.addOperationMetrics(HttpClientMetrics.get())
-            .buildInstrumenter();
+        InstrumenterBuilder<ClassAndMethod, Object> builder = Instrumenter.builder(
+            openTelemetry, INSTRUMENTATION_NAME, spanNameExtractor);
+
+        builder.setInstrumentationVersion(OtelVersion.VERSION);
+        if (spanStatusExtractor != null) {
+            builder.setSpanStatusExtractor(spanStatusExtractor);
+        }
+        if (errorCauseExtractor != null) {
+            builder.setErrorCauseExtractor(errorCauseExtractor);
+        }
+        builder.addAttributesExtractors(attributesExtractors);
+        contextCustomizers.forEach(builder::addContextCustomizer);
+        operationListeners.forEach(builder::addOperationListener);
+        operationMetrics.forEach(builder::addOperationMetrics);
+        spanLinksExtractors.forEach(builder::addSpanLinksExtractor);
+
+        return builder.buildInstrumenter();
+    }
+
+    /**
+     * Builds the default {@link SpanNameExtractor}.
+     * @return the {@link CodeSpanNameExtractor}
+     */
+    @Internal
+    @Singleton
+    SpanNameExtractor<ClassAndMethod> defaultSpanNameExtractor() {
+        return CodeSpanNameExtractor.create(ClassAndMethod.codeAttributesGetter());
     }
 }

ff9cfaf2e1221a0649feb22d1bff8aac70c055a3
Nemanja Mikic <nemanja.mikic@oracle.com>
2026-05-06 00:31:33 +0200

    Address PR review feedback

---
+++ tracing-opentelemetry-ucp/src/main/java/io/micronaut/tracing/opentelemetry/instrument/ucp/ManagedUniversalConnectionPoolMetricsBinder.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2017-2026 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.tracing.opentelemetry.instrument.ucp;
+
+import io.micronaut.context.annotation.Context;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.exceptions.ConfigurationException;
+import io.micronaut.core.annotation.Internal;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.jdbc.DataSourceResolver;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Singleton;
+import oracle.ucp.UniversalConnectionPool;
+import oracle.ucp.UniversalConnectionPoolException;
+import oracle.ucp.admin.UniversalConnectionPoolManager;
+import oracle.ucp.jdbc.PoolDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Registers UCP pools created by Micronaut JDBC UCP so OpenTelemetry can collect metrics.
+ */
+@Context
+@Singleton
+@Internal
+@Requires(beans = {UniversalConnectionPoolManager.class, DataSource.class})
+final class ManagedUniversalConnectionPoolMetricsBinder {
+
+    private final OracleUcpTelemetryConfiguration oracleUcpTelemetryConfiguration;
+    private final DataSourceResolver dataSourceResolver;
+    private final List<UniversalConnectionPool> registeredPools = new ArrayList<>();
+    private final Set<String> registeredPoolNames = new HashSet<>();
+
+    ManagedUniversalConnectionPoolMetricsBinder(
+        OracleUcpTelemetryConfiguration oracleUcpTelemetryConfiguration,
+        UniversalConnectionPoolManager connectionPoolManager,
+        @Nullable DataSourceResolver dataSourceResolver,
+        Collection<DataSource> dataSources) {
+        this.oracleUcpTelemetryConfiguration = oracleUcpTelemetryConfiguration;
+        this.dataSourceResolver = dataSourceResolver == null ? DataSourceResolver.DEFAULT : dataSourceResolver;
+        for (DataSource dataSource : dataSources) {
+            register(connectionPoolManager, dataSource);
+        }
+    }
+
+    private void register(UniversalConnectionPoolManager connectionPoolManager, DataSource dataSource) {
+        DataSource resolvedDataSource = dataSourceResolver.resolve(dataSource);
+        if (resolvedDataSource instanceof PoolDataSource poolDataSource) {
+            register(connectionPoolManager, poolDataSource);
+        } else {
+            try {
+                if (resolvedDataSource.isWrapperFor(PoolDataSource.class)) {
+                    register(connectionPoolManager, resolvedDataSource.unwrap(PoolDataSource.class));
+                }
+            } catch (SQLException e) {
+                throw new ConfigurationException("Failed to unwrap PoolDataSource from DataSource bean", e);
+            }
+        }
+    }
+
+    private void register(UniversalConnectionPoolManager connectionPoolManager, PoolDataSource poolDataSource) {
+        String poolName = poolDataSource.getConnectionPoolName();
+        if (!registeredPoolNames.add(poolName)) {
+            return;
+        }
+        try {
+            UniversalConnectionPool connectionPool = connectionPoolManager.getConnectionPool(poolName);
+            oracleUcpTelemetryConfiguration.oracleUcpTelemetry.registerMetrics(connectionPool);
+            registeredPools.add(connectionPool);
+        } catch (UniversalConnectionPoolException e) {
+            throw new ConfigurationException(String.format("Failed to register metrics for UCP connection pool named: %s", poolName), e);
+        }
+    }
+
+    /**
+     * Unregisters metrics for managed UCP pools.
+     */
+    @PreDestroy
+    void close() {
+        for (UniversalConnectionPool connectionPool : registeredPools) {
+            oracleUcpTelemetryConfiguration.oracleUcpTelemetry.unregisterMetrics(connectionPool);
+        }
+    }
+}
--- tracing-opentelemetry-ucp/src/test/groovy/io/micronaut/tracing/opentelemetry/instrument/ucp/OracleUcpTelemetryBeanCreationSpec.groovy
+++ tracing-opentelemetry-ucp/src/test/groovy/io/micronaut/tracing/opentelemetry/instrument/ucp/OracleUcpTelemetryBeanCreationSpec.groovy
@@ -6,6 +6,7 @@ import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.sdk.metrics.data.MetricData
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader
 import oracle.ucp.UniversalConnectionPool
+import oracle.ucp.jdbc.PoolDataSource
 import spock.lang.Specification
 
 class OracleUcpTelemetryBeanCreationSpec extends Specification {
@@ -76,6 +77,34 @@ class OracleUcpTelemetryBeanCreationSpec extends Specification {
         ctx.close()
     }
 
+    void "test Oracle UCP telemetry registers metrics for Micronaut UCP datasource"() {
+        given:
+        String poolName = "real-ucp-pool"
+        ApplicationContext ctx = ApplicationContext.run([
+                "datasources.default.connection-pool-name": poolName,
+                "datasources.default.url": "jdbc:h2:mem:ucpSmoke;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE",
+                "datasources.default.username": "sa",
+                "datasources.default.connection-factory-class-name": "org.h2.jdbcx.JdbcDataSource",
+                "datasources.default.initial-pool-size": 0,
+                "datasources.default.min-pool-size": 0,
+                "datasources.default.max-pool-size": 7,
+        ])
+        def reader = ctx.getBean(InMemoryMetricReader)
+        def poolDataSource = ctx.getBean(PoolDataSource)
+
+        expect:
+        poolDataSource.getConnectionPoolName() == poolName
+
+        when:
+        def metrics = reader.collectAllMetrics()
+
+        then:
+        metricValue(metrics, CONNECTION_MAX_METRICS, poolName, null) == 7
+
+        cleanup:
+        ctx.close()
+    }
+
     void "test Oracle UCP telemetry disabled with property"() {
         given:
         ApplicationContext ctx = ApplicationContext.builder([
@@ -90,6 +119,7 @@ class OracleUcpTelemetryBeanCreationSpec extends Specification {
         then:
         universalConnectionPoolBeanEventListener.isEmpty()
         oracleUcpTelemetryConfiguration.isEmpty()
+        ctx.findBean(ManagedUniversalConnectionPoolMetricsBinder).isEmpty()
         ctx.getBean(UniversalConnectionPool)
         !hasUcpMetrics(reader.collectAllMetrics())
 
